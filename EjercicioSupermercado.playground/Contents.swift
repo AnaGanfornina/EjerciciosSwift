7cb28e0f7fb6922035ea054fd94c28ce720804db
Ana Ganfornina <anies.llamani@gmail.com>
2025-01-23 12:30:14 +0100

    Ejercicio supermercado

---
+++ EjercicioSupermercado.playground/Contents.swift
@@ -0,0 +1,64 @@
+import Cocoa
+
+//Paso 1
+
+let nameSuper = "Super Awesome Market"
+let adress = "Calle Ficticia, 123"
+let yearsOfExistence = 10
+let averageSatisfaction:Float  = 4.5
+var isOpen:Bool = true
+
+//Paso 2
+
+var products = ["manzanas","plátanos","leche","huevos","pan"]
+products.append("caramelos")
+
+//Paso 3
+
+var frequentClients = Set(["alice","bob","charlie"])
+frequentClients.insert("diana")
+frequentClients.remove("charlie")
+
+var isFrequent:Bool = true
+
+for client in frequentClients{
+    if client == "Alice"{
+        isFrequent = true
+    }else{
+        isFrequent = false
+    }
+}
+
+/*
+ //También lo hice como una funcion pero me daba un error que no comprendía
+ 
+func isFrequent(name:String)->Bool{
+    var result = false
+    for client in frequentClients{
+        if client == name{
+            result =  true
+        }
+    }
+    return result
+}
+   
+*/
+
+// Paso 4
+
+let weeklyOffer: (nameProduct:String,originalPrice:Float,discount:Int) = ("manzanas",2.99,20)
+
+let discountCalculated = weeklyOffer.originalPrice * Float(weeklyOffer.discount) / 100
+
+print(weeklyOffer.originalPrice - discountCalculated)
+
+// Paso 5
+
+var salesRecord:[String:Int] = ["manzanas":50,"platanos":30,"leche":20,"huevos":10]
+salesRecord["manzanas"] = 55
+salesRecord["huevos"] = 0
+
+for product in salesRecord{
+    print("\(product.0) = \(product.1)")
+}
+            
