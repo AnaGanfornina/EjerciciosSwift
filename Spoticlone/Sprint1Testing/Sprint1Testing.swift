c0fb0be0e54e0d2e2cd6420fb945ce233c4408f1
Ana Ganfornina <anies.llamani@gmail.com>
2025-01-31 21:29:58 +0100

    Spoticlone Familiarizacion (con test retocados) y reproductor (sin test)

--- Spoticlone/Sprint1Testing/Sprint1Testing.swift
+++ Spoticlone/Sprint1Testing/Sprint1Testing.swift
@@ -6,70 +6,171 @@
 //
 
 import Testing
+import Foundation
 
 class Sprint1Testing {
-    @Test func getSongsFromArtist() async throws {
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
-        let songs = SongsLoader(fromData: testData).songs
-        #expect(Main().getSongsFromArtist("The Weeknd", inSongs: songs).count == 1, "Se espera que al menos haya una canción de The Weeknd")
+    @Suite("getSongsFromArtist")
+    class GetSongsFromArtistTest{
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
+        
+        
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
+        
+        //Para cuando la lista tiene una canción de theWeekend
+        @Test func whenSongsContainOnTheWeekndSong_thenReturn1Song() async throws {
+            #expect(mainInstance.getSongsFromArtist("The Weeknd", inSongs: songs).count == 1, "Se espera que al menos haya una canción de The Weeknd")
+        }
+        //Para cuando la lista está vacía
+        @Test func whenSongsEmpty() async throws {
+            #expect(mainInstance.getSongsFromArtist("The Weeknd", inSongs: []).count == 0, "Se espera un array de canciones vacías para The Weeknd porque no hemos pasado ninguna canción en la función")
+        }
+        
+        @Test func whenSongsDoNotContainArcticMonkeysButIsNotEmpty_thenReturnEmpty() async throws {
+            try #require(songs.count > 0, "El array de canciones no puede estar vacío")
+            #expect(Main().getSongsFromArtist("Arctic Monkeys", inSongs: songs).count == 0, "Se espera que no haya ninguna canción de ArcticMonkeys ya que el array de canciones pasado no contiene ninguna canción de ArcticMonkeys")
+        }
+    }
+    @Suite("getSongLongerThan")
+    class GetSongLongerThanTest{
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
+        
+        
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
+        
+        
+        @Test func whenSongsIsLongerThan203000_thenReturn2Song() async throws {
+            
+            #expect(mainInstance.getSongLongerThan(minutes: 203000, inSongs: songs).count == 2,"Se espera que al menos haya una canción")
+        }
+        
+        
+        @Test func whenSongEmpty_thenReturnEmpty() async throws {
+            #expect(mainInstance.getSongLongerThan(minutes: 203000, inSongs: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
+        }
+    }
+    @Suite("getSongWhith")
+    class GetSongWhithTest{
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
+        
+        
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
+        
+        @Test func whenSongContainTagPride_thenReturn1() async throws {
+            
+            #expect(mainInstance.getSongWhith(tag: "pride", inSongs: songs).count == 1,"Se espera que al menos haya una canción con la etiqueta pride")
+        }
+        
+        @Test func whenEmptySong_ReturnEmpty() async throws {
+            #expect(mainInstance.getSongWhith(tag: "pride", inSongs: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
+        }
+
     }
     
-    @Test func getSongsFromArtistWithEmptySong() async throws {
-        #expect(Main().getSongsFromArtist("The Weeknd", inSongs: []).count == 0, "Se espera un array de canciones vacías para The Weeknd porque no hemos pasado ninguna canción en la función")
-    }
-    @Test func getSongLongerThan() async throws {
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!   //Esto no se muuy bien qué es
-        let songs = SongsLoader(fromData: testData).songs
+    @Suite("getSongFor")
+    class GetSongForTest {
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
         
-        #expect(Main().getSongLongerThan(minutes: 203000, inSongs: songs).count == 2,"Se espera que al menos haya una canción")
+        
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
+        
+        @Test func whenBPMIs90_100_thenReturn2Songs () async throws {
+            #expect(mainInstance.getSongFor(BPM: (90, 100), inSong: songs).count == 2, "Se espera que haya 3 canciones")
+        }
+        
+        @Test func whenEmptySong_ReturnEmpty() async throws {
+            #expect(mainInstance.getSongFor(BPM: (90, 100), inSong: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
+        }
     }
     
-    
-    @Test func getSongLongerThanWithEmptySong() async throws {
-        #expect(Main().getSongLongerThan(minutes: 203000, inSongs: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
-    }
-    
-    @Test func getSongWhith() async throws {
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+    @Suite("countSongsForGenere")
+    class CountSongsForGenereTest {
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
         
-        let songs = SongsLoader(fromData: testData).songs
         
-        #expect(Main().getSongWhith(tag: "pride", inSongs: songs).count == 1,"Se espera que al menos haya una canción con la etiqueta pride")
-    }
-    
-    @Test func getSongWhithTagEmptySong() async throws {
-        #expect(Main().getSongWhith(tag: "pride", inSongs: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
-    }
-    
-    @Test func getSongFor() async throws {
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
         
-        let songs = SongsLoader(fromData: testData).songs
+        @Test func whenSongsContainGenerePop_thenReturn2() async throws {
+            #expect(mainInstance.countSongsForGenere(genre: "Pop", inSong: songs) == 2,"Se espera que haya 2 canciones de pop")
+        }
+        @Test func whenSongEmpty_thenReturnEmpty() async throws {
+            #expect(mainInstance.countSongsForGenere(genre: "Pop", inSong: []) == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
+        }
         
-        #expect(Main().getSongFor(BPM: (90, 100), inSong: songs).count == 2, "Se espera que haya 3 canciones")
-    }
-    
-    @Test func getSongForEmptySong() async throws {
-        #expect(Main().getSongFor(BPM: (90, 100), inSong: []).count == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
     }
     
-    @Test func countSongsForGenere() async throws {
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+    @Suite("countAverageOfPopularity")
+    class CountAverageOfPopularityTest {
+        var testData: Data?
+        var songs: [Song]
+        var mainInstance: Main
         
-        let songs = SongsLoader(fromData: testData).songs
         
-        #expect(Main().countSongsForGenere(genre: "Pop", inSong: songs) == 2,"Se espera que haya 2 canciones de pop")
-    }
-    @Test func countSongsForGenereEmptySong() async throws {
-        #expect(Main().countSongsForGenere(genre: "Pop", inSong: []) == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
-    }
-    @Test func countAverageOfPopularity() async throws{
-        let testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+        init() {
+            testData = TestData.getFiveSongs() ?? "".data(using: .utf8)!
+            songs = SongsLoader(fromData: testData).songs
+            mainInstance = Main()
+        }
+        deinit {
+            testData = nil
+            songs = []
+        }
         
-        let songs = SongsLoader(fromData: testData).songs
-        #expect(Main().countAverageOfPopularity(inSongs: songs) == 91,"Se espera que la popularidad promedio sea de 91")
+        @Test func whenAverageIs91() async throws{
+            #expect(mainInstance.countAverageOfPopularity(inSongs: songs) == 91,"Se espera que la popularidad promedio sea de 91")
+        }
+        @Test func whenSongEmpty_thenReturnEmpty() async throws {
+            #expect(mainInstance.countAverageOfPopularity(inSongs: []) == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
+        }
     }
-    @Test func countAverageOfPopularityEmptySong() async throws {
-        #expect(Main().countAverageOfPopularity(inSongs: []) == 0,"Se espera un array de canciones vacío al no haberle pasado ninguna canción")
-    }
+    
 }
